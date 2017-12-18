edf5bc1222372c641a7a5f922a4e8c9ba90403ed
lumos <630212894@qq.com>
2017-12-18 13:30:08 +0800

    em

--- Floyd-Warshall/Floyd-Warshall.cpp
+++ Floyd-Warshall/Floyd-Warshall.cpp
@@ -8,7 +8,9 @@ int path[MAX][MAX];
 int main(void)
 {
 	int i=0;
+	int m,n;//m number of points, n number of paths
 	ifstream  readFile("D:\\Floyd_Test.txt");
+	readFile>>m>>n;
 	while(!readFile.eof())//read file
 	{
 		readFile>>p[i].x>>p[i].y;
